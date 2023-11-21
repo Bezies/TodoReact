7a3d8b20cd25fe1fe1d4ce875ac1350465f170d4
Quentin Bessiere <beziesdev@gmail.com>
2023-11-21 14:43:47 +0100

    test images

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,8 +11,8 @@ export default function Header() {
     <div
       className={`${
         darkmode.state
-          ? "h-1/3 flex flex-col items-center justify-center bg-[url('images/bg-mobile-dark.jpg')] bg-cover md:bg-[url('images/bg-desktop-dark.jpg')]"
-          : "h-1/3 flex flex-col items-center justify-center bg-[url('images/bg-mobile-light.jpg')] bg-cover md:bg-[url('images/bg-desktop-light.jpg')]"
+          ? "h-1/3 flex flex-col items-center justify-center bg-[url('public/images/bg-mobile-dark.jpg')] bg-cover md:bg-[url('public/images/bg-desktop-dark.jpg')]"
+          : "h-1/3 flex flex-col items-center justify-center bg-[url('public/images/bg-mobile-light.jpg')] bg-cover md:bg-[url('public/images/bg-desktop-light.jpg')]"
       }`}
     >
       <div className="flex w-4/5 md:w-1/3 justify-between">
@@ -25,7 +25,9 @@ export default function Header() {
             <img
               className="h-8 w-8"
               src={`${
-                darkmode.state ? "images/icon-sun.svg" : "images/icon-moon.svg"
+                darkmode.state
+                  ? "images/icon-sun.svg"
+                  : "public/images/icon-moon.svg"
               }`}
               alt=""
             />
